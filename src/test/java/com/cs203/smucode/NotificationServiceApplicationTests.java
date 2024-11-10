83875adaad76e065b93d0d3232cb6f59d154227f
jeremainecheong <132808191+jeremainecheong@users.noreply.github.com>
2024-11-10 23:58:31 +0800

    Update NotificationServiceApplicationTests.java

--- src/test/java/com/cs203/smucode/NotificationServiceApplicationTests.java
+++ src/test/java/com/cs203/smucode/NotificationServiceApplicationTests.java
@@ -1,9 +1,15 @@
 package com.cs203.smucode;
 
+import com.cs203.smucode.config.TestSecurityConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestSecurityConfiguration.class)
 class NotificationServiceApplicationTests {
 
 	@Test
